e0a6f73347e0e54db1cd2a87119aae77a552549a
amercader <amercadero@gmail.com>
2015-07-28 14:50:47 +0100

    First version of the custom schema
    Includes most of the fields in the spec
    
    TODO:
    * clarify point of contact and org
    * Topic mandatory
    * Theming

--- ckanext/opendatani/plugin.py
+++ ckanext/opendatani/plugin.py
@@ -11,3 +11,10 @@ class OpendataniPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'opendatani')
+
+        # Set fixed config values for this extension
+        config_['scheming.dataset_schemas'] = 'ckanext.opendatani:dataset_schema.json'
+        config_['scheming.dataset_fallback'] = True
+
+        config_['licenses_group_url'] = '{0}/licenses.json'.format(
+            config_['ckan.site_url'].rstrip('/'))
