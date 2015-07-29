a1d317e70fec26fbc47cdc5bb0ff45e2d53fb9f7
amercader <amercadero@gmail.com>
2015-07-29 14:16:26 +0100

    Polish schema and form
    * Add contact fields
    * Fix spellings and labels
    * Add validators for required fields, number of keywords
    * Reorder fields

--- ckanext/opendatani/plugin.py
+++ ckanext/opendatani/plugin.py
@@ -2,8 +2,12 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+_ = toolkit._
+
+
 class OpendataniPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IValidators)
 
     # IConfigurer
 
@@ -18,3 +22,44 @@ class OpendataniPlugin(plugins.SingletonPlugin):
 
         config_['licenses_group_url'] = '{0}/licenses.json'.format(
             config_['ckan.site_url'].rstrip('/'))
+
+    # IValidators
+
+    def get_validators(self):
+        return {
+            'at_least_n_choices': at_least_n_choices,
+            'at_least_n_tags': at_least_n_tags,
+        }
+
+
+def at_least_n_tags(number_of_tags):
+
+    def _callable(key, data, errors, context):
+        value = data.get(key)
+        _number_of_tags = int(number_of_tags)
+
+        value = value.split(',')
+
+        if len(value) < _number_of_tags:
+            msg = (_('Enter at least 1 keyword') if _number_of_tags == 1 else
+                   _('Enter at least {0} keywords'.format(_number_of_tags)))
+            errors[key].append(msg)
+
+    return _callable
+
+
+def at_least_n_choices(number_of_choices):
+
+    def _callable(key, data, errors, context):
+        value = data.get(key)
+        _number_of_choices = int(number_of_choices)
+
+        if not isinstance(value, list):
+            return
+
+        if len(value) < _number_of_choices:
+            msg = (_('Enter at least 1 choice') if _number_of_choices == 1 else
+                   _('Enter at least {0} choices'.format(_number_of_choices)))
+            errors[key].append(msg)
+
+    return _callable
