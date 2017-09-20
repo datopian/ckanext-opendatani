d235f4e9aee906a545af5a31348513bbade72e4c
Goran Maksimovski <goran.maksimovski@keitaro.com>
2017-09-20 13:29:41 +0200

    Add missing (hierarchy) helpers, add untracked file

--- ckanext/opendatani/plugin.py
+++ ckanext/opendatani/plugin.py
@@ -10,7 +10,6 @@ from ckan.logic.action.create import user_create as core_user_create
 from ckan.logic.action.update import user_update as core_user_update
 import ckan.lib.helpers as h
 import datetime as dt
-
 _ = toolkit._
 
 
@@ -57,6 +56,9 @@ class OpendataniPlugin(plugins.SingletonPlugin):
             'get_user_num_stale_datasets': get_user_num_stale_datasets,
             'group_list': group_list,
             'package_list': package_list,
+            'group_tree': group_tree,
+            'group_tree_filter': group_tree_filter,
+            'group_tree_highlight': group_tree_highlight,
         }
 
     # IRoutes
@@ -327,3 +329,56 @@ def group_list():
 def package_list():
     package_list = toolkit.get_action('package_list')({}, {})
     return package_list
+
+
+def group_tree(organizations=[], type_='organization'):
+    full_tree_list = toolkit.get_action('group_tree')({}, {'type': type_})
+
+    if not organizations:
+        return full_tree_list
+    else:
+        filtered_tree_list = group_tree_filter(organizations, full_tree_list)
+        return filtered_tree_list
+
+
+def group_tree_filter(organizations, group_tree_list, highlight=False):
+    # this method leaves only the sections of the tree corresponding to the list
+    # since it was developed for the users, all children organizations from the
+    # organizations in the list are included
+    def traverse_select_highlighted(group_tree, selection=[], highlight=False):
+        # add highlighted branches to the filtered tree
+        if group_tree['highlighted']:
+            # add to the selection and remove highlighting if necessary
+            if highlight:
+                selection += [group_tree]
+            else:
+                selection += group_tree_highlight([], [group_tree])
+        else:
+            # check if there is any highlighted child tree
+            for child in group_tree.get('children', []):
+                traverse_select_highlighted(child, selection)
+
+    filtered_tree = []
+    # first highlights all the organizations from the list in the three
+    for group in group_tree_highlight(organizations, group_tree_list):
+        traverse_select_highlighted(group, filtered_tree, highlight)
+
+    return filtered_tree
+
+
+def group_tree_highlight(organizations, group_tree_list):
+
+    def traverse_highlight(group_tree, name_list):
+        if group_tree.get('name', "") in name_list:
+            group_tree['highlighted'] = True
+        else:
+            group_tree['highlighted'] = False
+        for child in group_tree.get('children', []):
+            traverse_highlight(child, name_list)
+
+    selected_names = [o.get('name', None) for o in organizations]
+    print(selected_names)
+
+    for group in group_tree_list:
+        traverse_highlight(group, selected_names)
+    return group_tree_list
