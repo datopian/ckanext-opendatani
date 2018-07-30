aba8ac08eb0b8016a6efde21bf1d38e64dbb6a7b
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-07-30 16:26:14 +0200

    Add table_dictize

--- ckanext/opendatani/model.py
+++ ckanext/opendatani/model.py
@@ -3,6 +3,13 @@ from ckan.model.meta import  metadata, mapper, Session, engine
 from sqlalchemy import Table, Column, Index, types
 from ckan.model.types import make_uuid
 from sqlalchemy.engine.reflection import Inspector
+try:
+    from sqlalchemy.engine.result import RowProxy
+except ImportError:
+    from sqlalchemy.engine.base import RowProxy
+import json
+import datetime
+from sqlalchemy.orm import class_mapper
 
 sftp_logs_table = None
 
@@ -63,3 +70,47 @@ def define_sftp_logs_table():
         CkanextSFTPLogs,
         sftp_logs_table
     )
+
+
+def table_dictize(obj, context, **kw):
+    '''Get any model object and represent it as a dict'''
+    result_dict = {}
+
+    if isinstance(obj, RowProxy):
+        fields = obj.keys()
+    else:
+        ModelClass = obj.__class__
+        table = class_mapper(ModelClass).mapped_table
+        fields = [field.name for field in table.c]
+
+    for field in fields:
+        name = field
+        if name in ('current', 'expired_timestamp', 'expired_id'):
+            continue
+        if name == 'continuity_id':
+            continue
+        value = getattr(obj, name)
+        if name == 'extras' and value:
+            result_dict.update(json.loads(value))
+        elif value is None:
+            result_dict[name] = value
+        elif isinstance(value, dict):
+            result_dict[name] = value
+        elif isinstance(value, int):
+            result_dict[name] = value
+        elif isinstance(value, datetime.datetime):
+            result_dict[name] = value.isoformat()
+        elif isinstance(value, list):
+            result_dict[name] = value
+        else:
+            result_dict[name] = unicode(value)
+
+    result_dict.update(kw)
+
+    # HACK For optimisation to get metadata_modified created faster.
+
+    context['metadata_modified'] = max(result_dict.get('revision_timestamp',
+                                                       ''),
+                                       context.get('metadata_modified', ''))
+
+    return result_dict
