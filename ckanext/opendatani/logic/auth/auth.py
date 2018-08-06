de3fa1a0d837886a057536dcf780e817ef0be468
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-08-06 15:24:39 +0200

    Add new auth

---
+++ ckanext/opendatani/logic/auth/auth.py
@@ -0,0 +1,27 @@
+import ckan.logic as logic
+from ckan.plugins.toolkit import _
+
+check_access = logic.check_access
+
+
+def insert_sftp_log(context, data_dict):
+    '''
+            Authorization check for inserting sftp logs
+        '''
+    # sysadmins only
+    return {'success': False}
+
+
+def list_sftp_logs(context, data_dict):
+    '''
+            Authorization check for list sftp logs
+        '''
+    # logged in user only
+    user = context['user']
+
+    if user:
+        return {'success': True}
+    else:
+        message = _('Only registered users can view logs.')
+
+        return {'success': False, 'msg': message}
