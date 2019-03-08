9179f97ab603796b51f0c30a1f3831e77082fe88
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-03-08 13:58:29 +0400

    Minor adjustments for travis

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,4 +3,5 @@
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
-nosetests --nologcapture --with-pylons=subdir/test.ini --with-coverage --cover-package=ckanext.opendatani --cover-inclusive --cover-erase --cover-tests
+echo "Preparing tests"
+nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --with-coverage --cover-package=ckanext.opendatani --cover-inclusive --cover-erase --cover-tests -v ckanext/opendatani
