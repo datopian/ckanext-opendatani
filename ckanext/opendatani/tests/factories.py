ceaffed2f5e5eb0b28ea4943566124725e8a41fa
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-03-08 16:19:01 +0400

    remove comas

--- ckanext/opendatani/tests/factories.py
+++ ckanext/opendatani/tests/factories.py
@@ -2,10 +2,10 @@ from ckan.tests import factories
 
 
 class Dataset(factories.Dataset):
-    frequency= 'daily',
-    contact_name= 'boita',
-    license_id= 'Test',
-    topic_category= 'farming',
-    contact_email= 'Test',
-    lineage= 'Test',
-    notes= 'Test'
+    frequency = 'daily'
+    contact_name = 'boita'
+    license_id = 'Test'
+    topic_category = 'farming'
+    contact_email = 'Test'
+    lineage = 'Test'
+    notes = 'Test'
