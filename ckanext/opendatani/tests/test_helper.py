1dbeaa51b05130ad36735de4b6d480b9798a6a77
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-03-08 12:50:22 +0400

    initial test for helpers to run Travis

---
+++ ckanext/opendatani/tests/test_helper.py
@@ -0,0 +1,34 @@
+import nose
+from routes import url_for
+
+from ckan.tests import helpers, factories
+from ckan.plugins import toolkit
+
+from ckanext.opendatani import helpers
+
+class TestHelpers(helpers.FunctionalTestBase):
+
+    COMMON_ACTIVITY = {
+        'user_id': 'user',
+        'timestamp': '2019-03-07T11:38:10.801967',
+        'is_new': True,
+        'object_id': 'object-id',
+        'revision_id': 'revision-id',
+        'data': {
+            'package': factories.Dataset()
+        },
+        'id': 'activity-id',
+        'activity_type': 'changed package'
+    }
+
+    @classmethod
+    def teardown_class(cls):
+        super(TestHelpers, cls).teardown_class()
+        helpers.reset_db()
+
+    def test_get_snippet_actor(self):
+        user = factories.User()
+        user_id = user.id
+        COMMON_ACTIVITY['user_id'] = user_id
+        result = helpers.get_snippet_actor(COMMON_ACTIVITY, None)
+        assert user['name'] == result
