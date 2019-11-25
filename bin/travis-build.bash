748668c1181dff1e3941082bd401a5e55e12cf5b
Michael Polidori <spatiumestabsolutum@gmail.com>
2019-11-25 15:51:52 -0500

    try specific pbr and urllib3 versions

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -26,8 +26,8 @@ sed -i '/psycopg2/c\psycopg2' requirements.txt
 python setup.py develop
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
-pip install pbr -U
-pip install urllib3 -U
+pip install pbr=1.3
+pip install urllib3=1.7.1
 cd -
 
 echo "Initialising the database..."
