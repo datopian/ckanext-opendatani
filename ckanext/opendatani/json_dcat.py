8a231e16619191771f32eddc66de33bcd988151e
steveoni <steohenoni2@gmail.com>
2024-06-13 18:22:48 +0100

    update type

--- ckanext/opendatani/json_dcat.py
+++ ckanext/opendatani/json_dcat.py
@@ -252,7 +252,7 @@ class NsiraJSONHarvester(DCATHarvester):
                         'title': f"JSON {resource['href'].split('/')[-2]}",
                         'accessURL': resource['href'],
                         'downloadURL': resource['href'],
-                        'format': resource['type']+f"{resource['href'].split('/')[-2]}"
+                        'format': f"json{resource['href'].split('/')[-2]}"
                     })
 
                 else:
